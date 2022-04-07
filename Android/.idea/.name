1147852c25aa2e3b95a512678e9bb315b5a5bc99
t9koal02 <t9koal02@students.oamk.fi>
2022-04-07 09:14:54 +0300

    Android sovelluksen videon toisto firebasesta valmis

--- Android/.idea/.name
+++ Android/.idea/.name
@@ -1,1 +1,1 @@
-Project
+project
--- Android/app/src/main/java/project/project/MainActivity.java
+++ Android/app/src/main/java/project/project/MainActivity.java
@@ -1,14 +1,49 @@
 package project.project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView Mrecyclerview;
+    FirebaseDatabase database;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Mrecyclerview = findViewById(R.id.recyclerview_video);
+        Mrecyclerview.setHasFixedSize(true);
+        Mrecyclerview.setLayoutManager(new LinearLayoutManager(this));
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference("video");
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<member, ViewHolder> firebaseRecyclerAdapter =
+                new FirebaseRecyclerAdapter<member, ViewHolder> (
+                        member.class,
+                        R.layout.row,
+                        ViewHolder.class,
+                        reference
+                ) {
+                    @Override
+                    protected void populateViewHolder(ViewHolder viewHolder, member member, int i) {
+                        viewHolder.setVideo(getApplication(), member.getTitle(), member.getUrl());
+                    }
+                };
+
+        Mrecyclerview.setAdapter(firebaseRecyclerAdapter);
     }
 }
