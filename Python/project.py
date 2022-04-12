3cb3a604ba7e0c9b6148aa4c1cc67a567a8d84f8
t9koal02 <t9koal02@students.oamk.fi>
2022-04-12 15:20:48 +0300

    Storagen ja databasen linkitys valmis

--- Python/project.py
+++ Python/project.py
@@ -3,6 +3,7 @@ import datetime
 import serial
 import pyrebase
 import subprocess
+import requests
 from picamera import PiCamera
 from time import sleep
 from time import strftime
@@ -14,7 +15,7 @@ if __name__ == '__main__':
     firebaseConfig = {
       "apiKey": "AIzaSyDBlb8Lgzl1KWEqq8gmBfdM7myFDsLxiW8",
       "authDomain": "project-tvt20kmo-r2.firebaseapp.com",
-      "databaseURL": "gs://project-tvt20kmo-r2.appspot.com",
+      "databaseURL": "https://project-tvt20kmo-r2-default-rtdb.firebaseio.com",
       "projectId": "project-tvt20kmo-r2",
       "storageBucket": "project-tvt20kmo-r2.appspot.com",
       #messagingSenderId: "327198659484",
@@ -22,8 +23,9 @@ if __name__ == '__main__':
       #measurementId: "G-RJ7REQEYK1"
       "serviceAccount": "serviceAccountKey.json"  # Erillinen tiedosto Python -kansiossa
     }
-    firebase_storage = pyrebase.initialize_app(firebaseConfig)
-    storage = firebase_storage.storage()
+    firebase = pyrebase.initialize_app(firebaseConfig)
+    storage = firebase.storage()
+    database = firebase.database()
     muuttuja = True  # Apumuuttuja todellisen liikkeen tunnistamiselle
     if serConn.isOpen():
         print("{} connected".format(serConn.port))
@@ -33,23 +35,31 @@ if __name__ == '__main__':
                 print("liikettä")
                 while True:
                     if muuttuja == True:
-                        date = datetime.datetime.now().strftime('%d-%m-%Y_%H.%M.%S')  # Videon nimeksi tulee päivämäärä
+                        # Videon nimeksi tulee päivämäärä ja kellonaika:
+                        date = datetime.datetime.now().strftime('%d-%m-%Y_%H.%M.%S') # Haetaan päivä ja aika  
+                        filename1 = date + '.h264'  # Tehdään kaksi eri nimi muuttujaa eri muotoisille videoille
+                        filename2 = date + '.mp4'
                         print("video alkaa")
                         # Videon kuvaaminen ja tallentaminen oikeaan sijaintiin Raspberryllä:
                         camera.start_preview()
-                        camera.start_recording('/home/pi/Product-Design-And-Implementation/Python/' + date + '.h264')  # PiCamera tukee .h264 -formaattia
-                        sleep(30)
+                        camera.start_recording('/home/pi/Product-Design-And-Implementation/Python/' + filename1)  # PiCamera tukee .h264 -formaattia
+                        sleep(5)
                         camera.stop_recording()
                         camera.stop_preview()
                         print("video loppuu")
-                        
+
                         # Videon muuntaminen Androidin tukemaan MP4-formaattiin:
-                        command = ['MP4Box', '-add', date + '.h264', date + '.mp4']
+                        command = ['MP4Box', '-add', filename1, filename2]
                         subprocess.Popen(command)
                         
                         muuttuja = False
                         sleep(0.55)  # Videon muuntaminen kestää yli 0,5 sekuntia. Jos ei viivästytetä videon siirtäminen Firebaseen ei onnistu
-                        storage.child(date + '.mp4').put(date + '.mp4')  # Video Firebaseen
+                        
+                        storage.child(filename2).put(filename2)  # Video Firebaseen
+                        url = storage.child(filename2).get_url(None)  # Haetaan Storagesta videon URL
+                    
+                        data = {'title':filename2, 'url':url}  # Tallenetaan muuttujaan videon nimi ja url
+                        database.child("video").push(data)  # Pusketaan data realtime databaseen
                         sleep(0.01)
                         # Videot (.h264 ja .mp4) poistetaan Raspberryltä:
                         if os.path.exists(date + '.h264') and os.path.exists(date + '.mp4'):
